20d32d756edf4628b9648b54f355026e8a09a445
vimal-11 <vimalaadith.bs@gmail.com>
2022-07-20 17:48:31 +0530

    messagetypes and errors

--- dm/consumers/chat_consumer.py
+++ dm/consumers/chat_consumer.py
@@ -3,3 +3,16 @@ from typing import Optional, Dict, Tuple
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth.models import AbstractBaseUser
+
+from .db_operations import get_groups_to_add, get_unread_count, get_user_by_pk, get_file_by_id, get_message_by_id, \
+                            save_file_message, save_text_message, mark_message_as_read
+
+from .message_types import MessageTypes, MessageTypeMessageRead, MessageTypeFileMessage, MessageTypeTextMessage, \
+    OutgoingEventMessageRead, OutgoingEventNewTextMessage, OutgoingEventNewUnreadCount, OutgoingEventMessageIdCreated,\
+    OutgoingEventNewFileMessage, OutgoingEventIsTyping, OutgoingEventStoppedTyping, OutgoingEventWentOnline, OutgoingEventWentOffline
+
+from .errors import ErrorTypes, ErrorDescription
+from dm.models import MessageModel, UploadedFile
+from dm.serializers import serialize_file_model
+from django.conf import settings
+import logging
--- dm/consumers/db_operations.py
+++ dm/consumers/db_operations.py
@@ -25,6 +25,15 @@ def get_file_by_id(file_id: str) -> Awaitable[Optional[UploadedFile]]:
 
 
 @database_sync_to_async
+def get_message_by_id(mid: int) -> Awaitable[Optional[Tuple[str, str]]]:
+    msg: Optional[MessageModel] = MessageModel.objects.filter(id=mid).first()
+    if msg:
+        return str(msg.recipient.pk), str(msg.sender.pk)
+    else:
+        return None
+        
+
+@database_sync_to_async
 def mark_message_as_read(mid: int) -> Awaitable[None]:
     return MessageModel.objects.filter(id=mid).update(read=True)
 
