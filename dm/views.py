e05f7aa98cc17460179b5e86f594a55cd59f8482
vimal-11 <vimalaadith.bs@gmail.com>
2022-07-20 14:30:22 +0530

    implemented serializers

--- dm/views.py
+++ dm/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    UpdateView,
+    ListView,
 
-# Create your views here.
+)
+from .models import MessageModel, DialogsModel, UploadedFile
+from .serializers import serialize_message_model, serialize_dialog_model, serialize_file_model
+from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect, HttpResponseBadRequest
+from django.core.paginator import Page, Paginator
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser
+from django.urls import reverse_lazy
+from django.forms import ModelForm
+import json
+
