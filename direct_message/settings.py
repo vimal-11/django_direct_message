90caed8db4fffec1b31475f47e3a5bb84cdac1dc
vimal-11 <vimalaadith.bs@gmail.com>
2022-07-20 19:28:08 +0530

    tweaking code

--- direct_message/settings.py
+++ direct_message/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'direct_message.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'direct_message', 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
